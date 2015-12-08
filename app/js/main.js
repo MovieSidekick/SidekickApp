12fed13c9d659c4f56156ec1609abf454daadaae
Thomas Heaton <thomasaheaton@gmail.com>
2015-12-08 09:57:29 -0500

    Merging files

--- app/js/main.js
+++ app/js/main.js
@@ -294,8 +294,6 @@ _angular2['default'].module('app.movies', ['app.core']).controller('MoviesContro
 Object.defineProperty(exports, '__esModule', {
   value: true
 });
-<<<<<<< HEAD
-
 var MovieService = function MovieService($http, SERVER, $cookies) {
 
   var url = 'https://floating-mountain-2068.herokuapp.com/movies';
@@ -332,41 +330,6 @@ var MovieService = function MovieService($http, SERVER, $cookies) {
     return $http.post(url, { type: 'title', title: ourTitle }, SERVER);
     console.log(ourTitle);
   }
-=======
-var url = 'https://floating-mountain-2068.herokuapp.com/movies';
-
-undefined.getAllMovies = getAllMovies;
-undefined.getMovie = getMovie;
-
-function Movie(movie) {
-  this.Poster = movie.Poster;
-  this.Title = movie.Title;
-  this.Rated = movie.Rated;
-  this.Year = movie.Year;
-  this.Released = movie.Released;
-  this.Runtime = movie.Runtime;
-  this.Director = movie.Director;
-  this.Writer = movie.Writer;
-  this.Actors = movie.Actors;
-  this.Plot = movie.Plot;
-  this.Language = movie.Language;
-  this.Country = movie.Country;
-  this.Awards = movie.Awards;
-  this.Metascore = movie.Metascore;
-  this.imdbRating = movie.imdbRating;
-  this.imdbVotes = movie.imdbVotes;
-  this.imdbID = movie.imdbID;
-  this.Type = movie.Type;
-}
-
-function getAllMovies() {
-  return $http.get(url, SERVER);
-}
-
-function getMovie(ourTitle) {
-  return $http.post(url, { type: 'title', title: ourTitle }, SERVER);
-  console.log(ourTitle);
->>>>>>> 6478006d378d946ebec9c6769de3d35abc655ae3
 };
 
 MovieService.$inject = ['$http', 'SERVER', '$cookies'];
--- js/app-movies/services/movie.service.js
+++ js/app-movies/services/movie.service.js
@@ -1,41 +1,5 @@
 let MovieService = function($http, SERVER, $cookies) {
   
-  let url = 'https://floating-mountain-2068.herokuapp.com/movies';
-
-  this.getAllMovies   = getAllMovies;
-  this.getMovie       = getMovie;
-
-  function Movie (movie) {
-    this.Poster = movie.Poster;
-    this.Title = movie.Title;
-    this.Rated = movie.Rated;
-    this.Year = movie.Year;
-    this.Released = movie.Released;
-    this.Runtime = movie.Runtime;
-    this.Director = movie.Director;
-    this.Writer = movie.Writer;
-    this.Actors = movie.Actors;
-    this.Plot = movie.Plot;
-    this.Language = movie.Language;
-    this.Country = movie.Country;
-    this.Awards = movie.Awards;
-    this.Metascore = movie.Metascore;
-    this.imdbRating = movie.imdbRating;
-    this.imdbVotes = movie.imdbVotes;
-    this.imdbID = movie.imdbID;
-    this.Type = movie.Type;
-  }
-
-  function getAllMovies () {
-    return $http.get(url, SERVER);
-  }
-
-  function getMovie (ourTitle) {
-    return $http.post(url, { type: 'title', title: ourTitle}, SERVER);
-    console.log(ourTitle);
-  }
-
-  
  let url = 'https://floating-mountain-2068.herokuapp.com/movies';
 
  this.getAllMovies   = getAllMovies;
@@ -70,7 +34,7 @@ let MovieService = function($http, SERVER, $cookies) {
    return $http.post(url, { type: 'title', title: ourTitle}, SERVER);
    console.log(ourTitle);
  
-
+}
  
 };
 
